638f26a4c522c4af1acf733788b5f78606a06d76
Thiago Lins <taglins@gmail.com>
2021-08-29 20:11:14 -0300

    Validação de data de nascimento completa

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+import { valida } from "./valida.js";
+const inputs = document.querySelectorAll("input");
+inputs.forEach((input) => {
+  input.addEventListener("blur", (event) => {
+    valida(event.target);
+  });
+});
---
+++ assets/js/valida.js
@@ -0,0 +1,84 @@
+/**
+ * Pensando em um código que possa ser reaproveitado e tendo em vista
+ * os recursos, criaremos uma função 'genérica' que servira para validar todos os campos
+ * e abaixo um objeto onde conterá o dataSet do input e especificando qual função acionar
+ */
+export function valida(input) {
+  const inputType = input.dataset.type;
+  if (validadores[inputType]) {
+    validadores[inputType](input);
+  }
+
+  if (input.validity.valid) {
+    input.parentElement.classList.remove("input-container--invalido");
+    input.parentElement.querySelector(".input-mensagem-erro").innerHtml = "";
+  } else {
+    input.parentElement.classList.add("input-container--invalido");
+    input.parentElement.querySelector(".input-mensagem-erro").innerHtml =
+      mostrarMsgErro(inputType, input);
+  }
+}
+const validadores = {
+  dataNascimento: (input) => validaNascimento(input),
+};
+
+/**
+ * Estrutura de validação global
+ */
+// Mensagens de erro personalizada por tipo de erro
+const mensagensDeErro = {
+  nome: { valueMissing: "O campo nome não pode estar em branco" },
+  email: {
+    valueMissing: "O campo  e-mail não pode estar em branco",
+    typeMismatch: "O e-mail digitado não é válido!",
+  },
+  senha: {
+    valueMissing: "O campo senha não pode estar em branco ",
+    patternMisMatch:
+      "A Senha deve conter entre 8 a 12 caracteres e pelo menos 1 letra minuscula, 1 letra maiuscula e um número",
+  },
+  dataNascimento: {
+    valueMissing: "O campo data de nascimento não pode estar em branco",
+    customError: "Você deve ser maior de 18 anos para se cadastrar",
+  },
+  cpf: {
+    valueMissing: "O campo CPF não pode estar em branco",
+  },
+};
+const tiposDeErros = [
+  "valueMissing",
+  "typeMismatch",
+  "patternMisMatch",
+  "customError",
+];
+
+function mostrarMsgErro(inputType, input) {
+  let mensagem = "";
+  return mensagem;
+}
+/**
+ * Validação de Data de nascimento e maioridade
+ */
+function validaNascimento(input) {
+  //Transformar a String em Data
+  const dataRecebida = new Date(input.value);
+  let mensagem = "";
+  if (!maiorDeIdade(dataRecebida)) {
+    mensagem = "Você deve ser maior de 18 anos para se cadastrar";
+  }
+
+  return input.setCustomValidity(mensagem);
+}
+function maiorDeIdade(data) {
+  //Criando a data de Hoje
+  const dataAtual = new Date();
+  //Tratando a data recebida para verificar se é maior de 18
+  const dataMais18 = new Date(
+    data.getUTCFullYear() + 18,
+    data.getUTCMonth(),
+    data.getUTCDate()
+  );
+
+  //validando
+  return dataMais18 <= dataAtual;
+}
